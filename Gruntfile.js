161c88e8e9323cfd18861c4a3bbf90f437e761d7
Chris Green <dev@cjgreen.com.au>
2015-09-18 01:10:38 +1000

    Removing PMS lib and updating build script to download latest PMS lib on build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,58 @@
+var tarball = require('tarball-extract');
+
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            build: 'build',
+            temp: 'temp',
+            redundantFiles: 'temp/<%= pkg.name %>/lib/dsm_config'
+        },
+        copy: {
+            source: {
+                options: {
+                    mode: true
+                },
+                expand: true,
+                cwd: 'source/',
+                src: ['**', '!**/*.DS_Store'],
+                dest: 'temp/<%= pkg.name %>'
+            },
+        },
+        plex: grunt.file.readJSON(process.env.HOME + '/.plex'), // Read personal plex token from config file
+        curl: {
+            download: {
+                src: 'https://plex.tv/downloads/latest/1?channel=8&build=linux-synology-arm7&distro=synology&X-Plex-Token=<%= plex.XPlexToken %>',
+                dest: 'temp/PlexMediaServer.tar'
+            }
+        },
         compress: {
             zip: {
                 options: {
                     archive: 'build/<%= pkg.name %>-<%= pkg.version %>.zip'
                 },
-                src: ['<%= pkg.name %>/**', '!<%= pkg.name %>/**/*.DS_Store'],
+                expand: true,
+                cwd: 'temp/',
+                src: '<%= pkg.name %>/**'
             }
         }
     });
 
+    grunt.registerTask('extract', function() {
+        var done = this.async();
+        tarball.extractTarball('temp/PlexMediaServer.tar', 'temp/PlexMediaServer', function(err) {
+            if (err) grunt.fail.fatal('Failed to extract temp/PlexMediaServer.tar: ' + err);
+            tarball.extractTarball('temp/PlexMediaServer/package.tgz', 'temp/'+grunt.config('pkg.name')+'/lib', function(err) {
+                if (err) grunt.fail.fatal('Failed to extract temp/PlexMediaServer/package.tgz: ' + err);
+                done();
+            });
+        });
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-curl');
     grunt.loadNpmTasks('grunt-contrib-compress');
 
-    grunt.registerTask('default', ['compress']);
+    grunt.registerTask('default', ['clean:build', 'copy', 'curl', 'extract', 'clean:redundantFiles', 'compress', 'clean:temp']);
 };
